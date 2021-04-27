7d34c77451d80663f6c52fb27e580d7720549821
DotEmms <emma_danckaert@live.be>
2021-04-27 13:35:48 +0200

    first angular app

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../Hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm",
+    imgUrl: "./assets/cart.png",
+  }
+
+  isClicked = false;
+  toggle = false;
+  clicked = 0;
+
+  HandleClicked(){
+    this.isClicked = true;
+  }
+
+  HandleBtnClicked(){
+    if(this.clicked % 2 === 0){
+      this.toggle = true;
+    this.hero.imgUrl = "./assets/home.png";
+    }
+    else{
+      this.toggle = false;
+    this.hero.imgUrl = "./assets/cart.png";
+    }
+    this.clicked++;
+  }
 
   constructor() { }
 
