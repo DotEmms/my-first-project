08f039ddf2152f934c04d36e467cfe3a8e2f4620
DotEmms <emma_danckaert@live.be>
2021-04-28 13:09:44 +0200

    pipe method changed

--- src/app/mypipe.pipe.ts
+++ src/app/mypipe.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MypipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number): number {
+    return value - 10;
   }
 
 }
