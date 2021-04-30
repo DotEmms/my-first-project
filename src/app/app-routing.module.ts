9f0fa208f39c00522cfa14b84c953e33c92e3da7
DotEmms <emma_danckaert@live.be>
2021-04-30 15:21:42 +0200

    markup and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent},
   { path: 'detail/:id', component: HeroDetailComponent }
