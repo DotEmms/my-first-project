f5c631b848fadc67df672f85986655f1afc667c3
DotEmms <emma_danckaert@live.be>
2021-05-03 10:52:16 +0200

    inserting a http DB api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,19 +3,31 @@ import { Observable, of } from 'rxjs';
 import { Hero } from '../app/Hero';
 import { MessagesService } from './messages.service';
 import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private MessagesService:MessagesService) { }
+  constructor(private http: HttpClient, private MessagesService:MessagesService) { }
 
-  getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
-    this.MessagesService.addMessage("HeroService: Fetched heroes.")
-    return heroes;
+  private log(message: string) {
+    this.MessagesService.addMessage(`HeroService: ${message}`);
   }
+
+  private heroesUrl = 'api/heroes';
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  // Without DB
+  // getHeroes(): Observable<Hero[]>{
+  //   const heroes = of(HEROES);
+  //   this.MessagesService.addMessage("HeroService: Fetched heroes.")
+  //   return heroes;
+  // }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(x => x.id === id) as Hero;
